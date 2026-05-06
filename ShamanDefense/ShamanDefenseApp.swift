a723efce441812e88c159a91c8c40241526807dd
Fearkunn <richiedaryl@gmail.com>
2026-05-06 13:55:58 +0700

    Initial Commit

---
+++ ShamanDefense/ShamanDefenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShamanDefenseApp.swift
+//  ShamanDefense
+//
+//  Created by Richie Daryl Kwenandar on 06/05/26.
+//
+
+import SwiftUI
+
+@main
+struct ShamanDefenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
