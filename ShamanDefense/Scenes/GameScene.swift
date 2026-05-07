309f5a420edcf5aa57681eb96a799af6498f7ac6
Fearkunn <richiedaryl@gmail.com>
2026-05-07 14:11:22 +0700

    feat: adding path logic, wave logic, and human logic for game map

--- ShamanDefense/Scenes/GameScene.swift
+++ ShamanDefense/Scenes/GameScene.swift
@@ -5,3 +5,26 @@
 //  Created by Richie Daryl Kwenandar on 06/05/26.
 //
 
+import SpriteKit
+
+enum TileVariant {
+    case straightH, straightV
+    case cornerTR, cornerTL, cornerBR, cornerBL
+}
+
+class GameScene: SKScene {
+    
+    private let tileSize: CGFloat = 36
+    private var pathManager: PathManager!
+    private var waveManager: WaveManager!
+    
+    override func didMove(to view: SKView) {
+        backgroundColor = SKColor(red: 0.13, green: 0.11, blue: 0.16, alpha: 1)
+        
+        pathManager = PathManager(scene: self, tileSize: tileSize)
+        pathManager.setup()
+        
+        waveManager = WaveManager(scene: self, waypoints: pathManager.waypoints, tileSize: tileSize)
+        waveManager.start()
+    }
+}
